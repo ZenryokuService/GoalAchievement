0aecab8af5fc08d141b5d734c1b9b4df4e8c8a47
takk <yogotak45@gmail.com>
2019-06-06 23:01:48 +0900

    テストファーストで開発する。

---
+++ src/main/jp/zenryokuservice/goal/ap/prot/MokuhyoAp.java
@@ -0,0 +1,31 @@
+package jp.zenryokuservice.goal.ap.prot;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * 目標達成アプリのプロトタイプを作成する、以下の機能のみ実装。
+ * 1. 目標ブレーク機能
+ * 2. タスク一覧機能
+ * 3. 進捗率集計機能
+ * 
+ * @author takunoji
+ * 2019/06/05
+ */
+public class MokuhyoAp extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+	}
+
+	/**
+	 * 目標達成APプロトタイプを起動する
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		launch();
+	}
+}
+
+
