b1609150e3c34137404db7752c3a88aa5bfc3534
takk <yogotak45@gmail.com>
2019-06-08 20:44:21 +0900

    GitのWebhookテスト

--- src/main/jp/zenryokuservice/goal/ap/prot/MokuhyoAp.java
+++ src/main/jp/zenryokuservice/goal/ap/prot/MokuhyoAp.java
@@ -1,6 +1,7 @@
 package jp.zenryokuservice.goal.ap.prot;
 
 import javafx.application.Application;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 /**
@@ -16,7 +17,7 @@ public class MokuhyoAp extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+		Label label = new Label();
 	}
 
 	/**
