4f747f6af4dbe39ee7812410d978f106d16256eb
takk <yogotak45@gmail.com>
2019-06-08 21:02:01 +0900

    Webhookテスト3

---
+++ src/main/jp/zenryokuservice/goal/ap/prot/label/MkhObjLabel.java
@@ -0,0 +1,12 @@
+package jp.zenryokuservice.goal.ap.prot.label;
+
+import javafx.scene.control.Label;
+
+/**
+ * 目標オブジェクト用のラベル
+ * @author takunoji
+ * 2019/06/08
+ */
+public class MkhObjLabel extends Label {
+
+}
